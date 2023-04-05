b89043eaa61424868cb58ff82a555c6f3029ee68
hiique <hiique>
2023-04-05 23:34:20 +0000

    Finalizado pagamentos
    5/4/2023

--- src/etl/pagamentos.sql
+++ src/etl/pagamentos.sql
@@ -1,26 +1,44 @@
 -- Databricks notebook source
-SELECT *
-FROM silver.olist.pedido
-WHERE dtPedido < '2018-01-01' 
-AND dtPedido >= add_months('2018-01-01', -6)
+SELECT DISTINCT t1.idPedido, t2.idVendedor, t1.dtPedido
+FROM silver.olist.pedido t1
+
+LEFT JOIN silver.olist.item_pedido t2 ON t1.idPedido = t2.idPedido
+
+WHERE t1.dtPedido <'2018-01-01' AND t1.dtPedido >= ADD_MONTHS('2018-01-01', -6)
+AND idVendedor IS NOT NULL
 
 
 -- COMMAND ----------
 
-WITH tb_join AS (
+WITH tb_pedidos AS (
 
-  SELECT t2.*, t3.idVendedor
-  FROM silver.olist.pedido t1
+  SELECT 
+      DISTINCT 
+      t1.idPedido,
+      t2.idVendedor
 
-  LEFT JOIN silver.olist.pagamento_pedido t2
+  FROM silver.olist.pedido AS t1
+
+  LEFT JOIN silver.olist.item_pedido as t2
   ON t1.idPedido = t2.idPedido
 
-  LEFT JOIN silver.olist.item_pedido t3
-  ON t1.idPedido = t3.idPedido
-
-  WHERE t1.dtPedido < '2018-01-01' 
+  WHERE t1.dtPedido < '2018-01-01'
   AND t1.dtPedido >= add_months('2018-01-01', -6)
-  AND t3.idVendedor IS NOT NULL
+  AND idVendedor IS NOT NULL
+
+),
+
+tb_join AS (
+
+  SELECT 
+        t1.idVendedor,
+        t2.*         
+
+  FROM tb_pedidos AS t1
+
+  LEFT JOIN silver.olist.pagamento_pedido AS t2
+  ON t1.idPedido = t2.idPedido
+
 ),
 
 tb_group AS(
@@ -33,34 +51,64 @@ tb_group AS(
 
   GROUP BY idVendedor, descTipoPagamento
   ORDER BY idVendedor, descTipoPagamento
-)
+),
 
-SELECT idVendedor,
+tb_summary AS ( 
 
-SUM(CASE WHEN descTipoPagamento = 'boleto' THEN qtdePedidoMeioPagamento ELSE 0 END) as qtde_boleto_pedido,
-SUM(CASE WHEN descTipoPagamento = 'credit_card' THEN qtdePedidoMeioPagamento ELSE 0 END) as qtde_credit_card_pedido,
-SUM(CASE WHEN descTipoPagamento = 'voucher' THEN qtdePedidoMeioPagamento ELSE 0 END) as qtde_voucher_pedido,
-SUM(CASE WHEN descTipoPagamento = 'debit_card' THEN qtdePedidoMeioPagamento ELSE 0 END) as qtde_debit_card_pedido,
+  SELECT idVendedor,
 
-SUM(CASE WHEN descTipoPagamento = 'boleto' THEN vlPedidoMeioPagamento ELSE 0 END) as valor_boleto_pedido,
-SUM(CASE WHEN descTipoPagamento = 'credit_card' THEN vlPedidoMeioPagamento ELSE 0 END) as valor_credit_card_pedido,
-SUM(CASE WHEN descTipoPagamento = 'voucher' THEN vlPedidoMeioPagamento ELSE 0 END) as valor_voucher_pedido,
-SUM(CASE WHEN descTipoPagamento = 'debit_card' THEN vlPedidoMeioPagamento ELSE 0 END) as valor_debit_card_pedido,
+  SUM(CASE WHEN descTipoPagamento = 'boleto' THEN qtdePedidoMeioPagamento ELSE 0 END) as qtde_boleto_pedido,
+  SUM(CASE WHEN descTipoPagamento = 'credit_card' THEN qtdePedidoMeioPagamento ELSE 0 END) as qtde_credit_card_pedido,
+  SUM(CASE WHEN descTipoPagamento = 'voucher' THEN qtdePedidoMeioPagamento ELSE 0 END) as qtde_voucher_pedido,
+  SUM(CASE WHEN descTipoPagamento = 'debit_card' THEN qtdePedidoMeioPagamento ELSE 0 END) as qtde_debit_card_pedido,
 
-SUM(CASE WHEN descTipoPagamento = 'boleto' THEN qtdePedidoMeioPagamento ELSE 0 END) / SUM(qtdePedidoMeioPagamento) as pct_boleto_pedido,
-SUM(CASE WHEN descTipoPagamento = 'credit_card' THEN qtdePedidoMeioPagamento ELSE 0 END) / SUM(qtdePedidoMeioPagamento) as pct_credit_card_pedido,
-SUM(CASE WHEN descTipoPagamento = 'voucher' THEN qtdePedidoMeioPagamento ELSE 0 END) / SUM(qtdePedidoMeioPagamento) as pct_voucher_pedido,
-SUM(CASE WHEN descTipoPagamento = 'debit_card' THEN qtdePedidoMeioPagamento ELSE 0 END) / SUM(qtdePedidoMeioPagamento) as pct_debit_card_pedido,
+  SUM(CASE WHEN descTipoPagamento = 'boleto' THEN vlPedidoMeioPagamento ELSE 0 END) as valor_boleto_pedido,
+  SUM(CASE WHEN descTipoPagamento = 'credit_card' THEN vlPedidoMeioPagamento ELSE 0 END) as valor_credit_card_pedido,
+  SUM(CASE WHEN descTipoPagamento = 'voucher' THEN vlPedidoMeioPagamento ELSE 0 END) as valor_voucher_pedido,
+  SUM(CASE WHEN descTipoPagamento = 'debit_card' THEN vlPedidoMeioPagamento ELSE 0 END) as valor_debit_card_pedido,
 
-SUM(CASE WHEN descTipoPagamento = 'boleto' THEN vlPedidoMeioPagamento ELSE 0 END) / SUM(vlPedidoMeioPagamento) as pct_valor_boleto_pedido,
-SUM(CASE WHEN descTipoPagamento = 'credit_card' THEN vlPedidoMeioPagamento ELSE 0 END) / SUM(vlPedidoMeioPagamento) as pct_valor_credit_card_pedido,
-SUM(CASE WHEN descTipoPagamento = 'voucher' THEN vlPedidoMeioPagamento ELSE 0 END) / SUM(vlPedidoMeioPagamento) as pct_valor_voucher_pedido,
-SUM(CASE WHEN descTipoPagamento = 'debit_card' THEN vlPedidoMeioPagamento ELSE 0 END) / SUM(vlPedidoMeioPagamento) as pct_valor_debit_card_pedido
+  SUM(CASE WHEN descTipoPagamento = 'boleto' THEN qtdePedidoMeioPagamento ELSE 0 END) / SUM(qtdePedidoMeioPagamento) as pct_boleto_pedido,
+  SUM(CASE WHEN descTipoPagamento = 'credit_card' THEN qtdePedidoMeioPagamento ELSE 0 END) / SUM(qtdePedidoMeioPagamento) as pct_credit_card_pedido,
+  SUM(CASE WHEN descTipoPagamento = 'voucher' THEN qtdePedidoMeioPagamento ELSE 0 END) / SUM(qtdePedidoMeioPagamento) as pct_voucher_pedido,
+  SUM(CASE WHEN descTipoPagamento = 'debit_card' THEN qtdePedidoMeioPagamento ELSE 0 END) / SUM(qtdePedidoMeioPagamento) as pct_debit_card_pedido,
+
+  SUM(CASE WHEN descTipoPagamento = 'boleto' THEN vlPedidoMeioPagamento ELSE 0 END) / SUM(vlPedidoMeioPagamento) as pct_valor_boleto_pedido,
+  SUM(CASE WHEN descTipoPagamento = 'credit_card' THEN vlPedidoMeioPagamento ELSE 0 END) / SUM(vlPedidoMeioPagamento) as pct_valor_credit_card_pedido,
+  SUM(CASE WHEN descTipoPagamento = 'voucher' THEN vlPedidoMeioPagamento ELSE 0 END) / SUM(vlPedidoMeioPagamento) as pct_valor_voucher_pedido,
+  SUM(CASE WHEN descTipoPagamento = 'debit_card' THEN vlPedidoMeioPagamento ELSE 0 END) / SUM(vlPedidoMeioPagamento) as pct_valor_debit_card_pedido
 
 
 
 FROM tb_group
 GROUP BY 1
+),
+
+tb_cartao AS (
+
+  SELECT idVendedor,
+         AVG(nrParcelas) AS avgQtdeParcelas,
+         PERCENTILE(nrParcelas, 0.5) AS medianQtdeParcelas,
+         MAX(nrParcelas) AS maxQtdeParcelas,
+         MIN(nrParcelas) AS minQtdeParcelas
+
+  FROM tb_join
+  WHERE descTipoPagamento = 'credit_card'
+  GROUP BY idVendedor
+
+)
+
+SELECT 
+       '2018-01-01' AS dtReference,
+       t1.*,
+       t2.avgQtdeParcelas,
+       t2.medianQtdeParcelas,
+       t2.maxQtdeParcelas,
+       t2.minQtdeParcelas
+
+FROM tb_summary as t1
+
+LEFT JOIN tb_cartao as t2
+ON t1.idVendedor = t2.idVendedor
 
 -- COMMAND ----------
 
